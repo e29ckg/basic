26d041cfa991a57da47ef67010036d833b38e502
e29ckg <e29ckg@gmail.com>
2019-08-30 11:52:48 +0700

    Update m190823_134321_ven_com.php

--- migrations/m190823_134321_ven_com.php
+++ migrations/m190823_134321_ven_com.php
@@ -21,6 +21,7 @@ class m190823_134321_ven_com extends Migration
         $this->createTable('ven_com', [
             'id' => $this->primaryKey(),
             'ven_com_num' => $this->string(),
+            'ven_com_date' => $this->string(),
             'ven_time' => $this->string(),
             'ven_month' => $this->string(),
             'ven_com_name' => $this->string(),
